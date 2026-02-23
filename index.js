2b6e902e8e295af112c1e5bf7078133cdd97dbf7
sampu393@gmail.com <sampu393@gmail.com>
2026-02-23 23:21:22 +0530

    add validation log

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const greet = require("./utils");
 
 console.log(greet("Sampath"));
 console.log("Starting validation");
+
+console.log("Starting validation");
+function validateAge(age) {
+  return age > 18;
+}
